2beab27ad5cb8f478d312bd34c4dd3b2ecd92b62
AhnDo0 <51705063+AhnDo0@users.noreply.github.com>
2024-04-05 02:41:20 +0900

    Title: 빙산의 일각, Time: 5031ms, Memory: 33MB, Status: Time Limit Exceed - CodeTree

--- 240405/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240405/빙산의 일각/the-tip-of-the-iceberg.py
@@ -3,6 +3,7 @@ heights = []
 height_type = [0]
 count = 0
 max_mountain = 0
+checked = [0] * (N+2)
 
 for i in range(N):
     h = int(input())
@@ -10,20 +11,23 @@ for i in range(N):
         height_type.append(h)
     heights.append(h)
 
-for water in reversed(height_type):
+count = 0
+height_type.sort()
+height_type.pop()
+height_type.sort(reverse=True)
+
+for water in height_type:
     is_new = True
-    count = 0
-    for summit in heights:
-        if summit > water and is_new:
-            count += 1
-            is_new = False
-        else:
-            if summit <= water:
-                is_new = True
+    for summit in range(1, len(heights)+1):
+        if checked[summit]:
             continue
-        if max_mountain < count:
-            max_mountain = count
-    if max_mountain > count:
-        break
+        elif heights[summit-1] > water:
+            if not checked[summit - 1] and not checked[summit+1]:
+                count += 1
+            elif checked[summit - 1] and checked[summit+1]:
+                count -= 1
+            checked[summit] = 1
+    if max_mountain < count:
+        max_mountain = count
 
 print(max_mountain)
